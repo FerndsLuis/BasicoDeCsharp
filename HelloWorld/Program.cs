5678b64c3cabef5b96cb4d13437c9d417a768be3
Luís Fernandes <hsantos.luis@outlook.com>
2022-01-25 21:45:18 -0300

    Bloco 3 - Variável  'sem tipo' e tipagem dinâmica

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,18 +9,40 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int segundaGuerraMundial = 1942;
-            string cor_favorita = "vermelho";
-            float velocidadeF1 = 294.48f;
-            bool segundaGuerrraMundialAconteceu = true;
+            /* 
+             * Bloco 1º - conceitos básicos de variáveis
+             * 
+            int segundaGuerraMundial = 1942; //Declarando váriavel do tipo int
+            string cor_favorita = "vermelho"; //Declarando váriavel do tipo string
+            float velocidadeF1 = 294.48f; //Declarando váriavel do tipo float - precisa do "f"
+            bool segundaGuerrraMundialAconteceu = true; //Declarando váriavel do tipo boleano
 
-
+            //Exibindo variáveis
             Console.WriteLine(segundaGuerraMundial);
             Console.WriteLine(cor_favorita);
             Console.WriteLine(velocidadeF1);
             Console.WriteLine(segundaGuerrraMundialAconteceu);
+            */
 
-            Console.ReadLine();
+            /* 
+            * Bloco 2º - declarando variável sem especificar o tipo
+            * 
+            *
+            var cor_favorita = "Vermelho"; // automáticamente é tipada como string, não pode ser alterada para um int, por exemplo
+            var modeloDoProduto = 1010; // automáticamente é tipada como int, não pode ser alterada para um string, por exemplo
+            */
+
+            /* 
+            * Bloco 3º - declarando variável dinâmica
+            *
+            */
+
+            dynamic cor_favorita = "Vermelho"; // o tipo dela pode ser alterado
+            cor_favorita = 10;
+            Console.WriteLine(cor_favorita);
+
+
+            Console.ReadLine(); //para o console não fechar ao fim da execução 
         }
     }
 }
