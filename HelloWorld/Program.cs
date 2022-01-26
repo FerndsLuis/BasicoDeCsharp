31a91cc584abe753f18ac80620c9873a79c24d24
Luís Fernandes <hsantos.luis@outlook.com>
2022-01-25 21:33:33 -0300

    Parte essencial sobre variáveis

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundaGuerraMundial = 1942;
+            string cor_favorita = "vermelho";
+            float velocidadeF1 = 294.48f;
+            bool segundaGuerrraMundialAconteceu = true;
+
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(cor_favorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segundaGuerrraMundialAconteceu);
+
+            Console.ReadLine();
+        }
+    }
+}
